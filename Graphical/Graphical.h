3ba024fbc8be13e807b824cca3d0d982ce187cc6
Rob <robbor911@gmail.com>
2018-03-01 14:02:51 -0600

    Initial Commit

---
+++ Graphical/Graphical.h
@@ -0,0 +1,19 @@
+//
+//  Graphical.h
+//  Graphical
+//
+//  Created by Robert Caraway on 3/1/18.
+//  Copyright © 2018 Rob Caraway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graphical.
+FOUNDATION_EXPORT double GraphicalVersionNumber;
+
+//! Project version string for Graphical.
+FOUNDATION_EXPORT const unsigned char GraphicalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graphical/PublicHeader.h>
+
+
